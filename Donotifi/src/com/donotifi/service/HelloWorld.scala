5f256db75aef68a4300c51988c574601209c4784
javeedchida <javeed.chida@gmail.com>
2016-09-21 17:43:16 -0500

    scala learning in progress

--- Donotifi/src/com/donotifi/service/HelloWorld.scala
+++ Donotifi/src/com/donotifi/service/HelloWorld.scala
@@ -1,8 +1,58 @@
-
 package com.donotifi.service
+import java.util.{Date, Locale}
+import java.text.DateFormat
+import java.text.DateFormat._
 
 object HelloWorld {
   def main(args: Array[String] ) {
     println("Hello, world!")
   }
 }
+
+object FrenchDate {
+  def main(args: Array[String]) {
+    val now = new Date
+    val df = getDateInstance(LONG, Locale.FRANCE)
+    println(df format now)
+  }
+}
+
+object Timer {
+  def oncePerSecond(callback: () => Unit) {
+    while (true) { callback(); Thread sleep 1000 }
+  }
+  def timeFlies() {
+    println("time flies like an arrow...")
+  }
+  def main(args: Array[String]) {
+    oncePerSecond(timeFlies)
+  }
+}
+
+object TimerAnonymous {
+  def oncePerSecond(callback: () => Unit) {
+    while (true) { callback(); Thread sleep 1000 }
+  }
+  def main(args: Array[String]) {
+    oncePerSecond(() =>
+      println("time flies like an arrow..."))
+  }
+}
+
+class Complex(real: Double, imaginary: Double) {
+  def re = real
+  def im = imaginary
+  override def toString() =
+    "" + re + (if (im < 0) "" else "+") + im + "i"
+}
+
+object ComplexNumbers {
+  def main(args: Array[String]) {
+    val c = new Complex(1.2, 3.4)
+    println("imaginary part: " + c.im)
+    println(c)
+  }
+}
+
+
+
